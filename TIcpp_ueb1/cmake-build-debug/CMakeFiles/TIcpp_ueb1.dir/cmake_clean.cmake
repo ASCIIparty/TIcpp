3ddcdbbf28f890be1efd01d383d29b0a3cc3d057
schwesterBob <s55812@beuth-hochschule.de>
2017-04-20 19:44:26 +0200

    added Aufgabe 2

---
+++ TIcpp_ueb1/cmake-build-debug/CMakeFiles/TIcpp_ueb1.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/TIcpp_ueb1.dir/TDate.cpp.o"
+  "CMakeFiles/TIcpp_ueb1.dir/TTime.cpp.o"
+  "CMakeFiles/TIcpp_ueb1.dir/Ueb1.cpp.o"
+  "TIcpp_ueb1.pdb"
+  "TIcpp_ueb1"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/TIcpp_ueb1.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
