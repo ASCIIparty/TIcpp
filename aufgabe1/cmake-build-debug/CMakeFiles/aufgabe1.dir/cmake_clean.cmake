1bea22facefd6fae555f43d4f0e5b9f453f0af37
schwesterBob <s55812@beuth-hochschule.de>
2017-04-20 20:00:49 +0200

    testest kommenter

---
+++ aufgabe1/cmake-build-debug/CMakeFiles/aufgabe1.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/aufgabe1.dir/main.cpp.o"
+  "CMakeFiles/aufgabe1.dir/tdate.cpp.o"
+  "CMakeFiles/aufgabe1.dir/ttime.cpp.o"
+  "aufgabe1.pdb"
+  "aufgabe1"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/aufgabe1.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
